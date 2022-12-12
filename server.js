c708b1f2c6fe0235ce6a9fdb1e27dfbb5e07d952
Jessica9126 <912675127@qq.com>
2022-12-12 13:30:07 -0500

    Final

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
+const bcrypt = require('bcryptjs');
 //const schema = require('./schema');
 const cors = require('cors');
 const { buildSchema } = require('graphql');
@@ -32,18 +33,51 @@ app.use(
                 password: String!
             }
 
+            type UserInfo{
+                _id: ID!
+                email: String!
+                password: String!
+                nickname: String!
+                status: Boolean!
+            }
+
+            type Blog{
+                _id: ID!
+                owner: String!
+                title: String!
+                content: String!
+                isliked: Boolean!
+                picture: String!
+                
+            }
+
             input UserInput {
                 email: String!
                 password: String!
             }
 
+            input UserInfoInput {
+                email: String!
+                password: String!
+                nickname: String!
+            }
+
+            input BlogInput {
+                email: String!
+                owner: String!
+                title: String!
+                content: String!
+            }
+
             type RootQuery{
                 users: [User!]!
-
+                user(email: String!, password: String!): User!
             }
 
             type RootMutation{
                 createUser(userInput: UserInput): User
+                createUserInfo(userinfoInput: UserInfoInput): UserInfo
+                createBlog(blogInput: BlogInput): Blog
             }
 
             schema{
@@ -65,6 +99,17 @@ app.use(
                     throw err;
                 });
             },
+            user: async ({ email, password }) => {
+                const user = await User.findOne({ email: email });
+                if (!user) {
+                  throw new Error('User does not exist!');
+                }
+                //const isEqual = await bcrypt.compare(password, user.password);
+                if (password != user.password) {
+                  throw new Error('Password is incorrect!');
+                }
+                return {...user._doc};
+            },
             createUser: args => {
                 // const user = {
                 //     _id: Math.random().toString,
@@ -83,6 +128,35 @@ app.use(
                     console.log(err);
                     throw err;
                 });
+            },
+            createUserInfo: args => {
+                const userinfo = new UserInfo({
+                    email: args.userinfoInput.email,
+                    password: args.userinfoInput.password,
+                    nickname: args.userinfoInput.nickname,
+                });
+                return userinfo.save().then(result =>{
+                    console.log(result);
+                    return {...result._doc};
+                }).catch(err=>{
+                    console.log(err);
+                    throw err;
+                });
+            },
+            createBlog: args => {
+                const blog = new Blog({
+                    email: args.blogInput.email,
+                    owner: args.blogInput.owner,
+                    title: args.blogInput.title,
+                    content: args.blogInput.content,
+                });
+                return blog.save().then(result =>{
+                    console.log(result);
+                    return {...result._doc};
+                }).catch(err=>{
+                    console.log(err);
+                    throw err;
+                });
             }
         },
         graphiql: true
@@ -91,7 +165,8 @@ app.use(
 
 const PORT = process.env.PORT || 7000;
 
-mongoose.connect(`mongodb+srv://root:root@atlascluster.hbnuqma.mongodb.net/UserInfo?retryWrites=true&w=majority`)
+mongoose.connect(`mongodb+srv://root:root@atlascluster.hbnuqma.mongodb.net/
+UserInfo?retryWrites=true&w=majority`)
         .then(()=>{
             app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
         })
